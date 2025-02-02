768e2acf347ab1840db78b535b134c17d9a37b6c
DantehCheung <dantehcheung@gmail.com>
2025-02-02 23:58:16 +0800

    first push

---
+++ src/main/kotlin/com/fyp/crms_backend/CrmsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.fyp.crms_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrmsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrmsBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/fyp/crms_backend/CrmsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fyp.crms_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrmsBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
