20d95b9b34aac7ddbf2928b8830c58c93288e3f9
ivecw2425fyp <ivecw2425fyp@gmail.com>
2025-03-24 22:21:11 +0800

    滿懷“love”接近中！ campus remove delete function

--- src/main/kotlin/com/fyp/crms_backend/dto/campus/DeleteCampusResponse.kt
+++
@@ -1,4 +0,0 @@
-package com.fyp.crms_backend.dto.campus
-
-class DeleteCampusResponse {
-}
--- src/main/kotlin/com/fyp/crms_backend/repository/CampusRepository.kt
+++ src/main/kotlin/com/fyp/crms_backend/repository/CampusRepository.kt
@@ -99,4 +99,5 @@ class CampusRepository(jdbcTemplate: JdbcTemplate) : ApiRepository(jdbcTemplate)
         }
     }
 
+
 }
