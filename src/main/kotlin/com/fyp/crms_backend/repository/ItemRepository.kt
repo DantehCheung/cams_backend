c9784ec7ced49b184fe432d2ed102b25c760c874
DantehCheung <dantehcheung@gmail.com>
2025-03-25 16:01:40 +0800

    done by eric ho, get items change change change

--- src/main/kotlin/com/fyp/crms_backend/repository/ItemRepository.kt
+++ src/main/kotlin/com/fyp/crms_backend/repository/ItemRepository.kt
@@ -9,18 +9,17 @@ import org.springframework.stereotype.Repository
 
 
 @Repository
-class ItemRepository(jdbcTemplate: JdbcTemplate) : ApiRepository(jdbcTemplate) {
+class ItemRepository(override val jdbcTemplate: JdbcTemplate) : ApiRepository(jdbcTemplate) {
 
     private val rowMapper = RowMapper<CAMSDB.Device> { rs, _ ->
         CAMSDB.Device(
-
          deviceID = rs.getInt("deviceID"),
          deviceName = rs.getString("deviceName"),
          price = rs.getBigDecimal("price"),
          orderDate = rs.getDate("orderDate")?.toLocalDate(),
          arriveDate = rs.getDate("arriveDate")?.toLocalDate(),
          maintenanceDate = rs.getDate("maintenanceDate")?.toLocalDate(),
-         roomID = rs.getInt("roomID "),
+         roomID = rs.getInt("roomID"),
          state = rs.getString("state")?.singleOrNull(),
          remark = rs.getString("remark"),
         )
@@ -29,8 +28,9 @@ class ItemRepository(jdbcTemplate: JdbcTemplate) : ApiRepository(jdbcTemplate) {
     fun fetchData(CNA: String,roomID: Int): List<CAMSDB.Device> {
 
         return super.APIprocess(CNA, "get Device Data") {
+
             val result: List<CAMSDB.Device> = jdbcTemplate.query(
-                """SELECT deviceID,deviceName,price,orderDate,arriveDate,maintenanceDate,device.roomID,state,remark from device inner join room where  device.roomID = room.roomID AND CNA = ? AND room.roomID = ?""",
+                """SELECT deviceID,deviceName,price,orderDate,arriveDate,maintenanceDate,device.roomID,state,remark from device,room,user where  device.roomID = room.roomID AND user.CNA = ? AND room.roomID = ?""",
                 rowMapper,
                 CNA,
                 roomID
