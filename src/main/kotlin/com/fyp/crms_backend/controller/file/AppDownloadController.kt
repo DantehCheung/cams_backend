1874ee95f463685b84b8c06c1957934d307f1a48
ivecw2425fyp <ivecw2425fyp@gmail.com>
2025-04-17 20:01:11 +0800

    Just Believe!!!

--- src/main/kotlin/com/fyp/crms_backend/controller/file/AppDownloadController.kt
+++ src/main/kotlin/com/fyp/crms_backend/controller/file/AppDownloadController.kt
@@ -6,11 +6,7 @@ import org.springframework.core.io.FileSystemResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.io.File
 
 
